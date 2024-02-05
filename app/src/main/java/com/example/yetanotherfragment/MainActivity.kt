1e78b9d9d81827f4440a44b916c967a54cae4ee6
Kapoor-Baroos666 <amrilkapoor@gmail.com>
2024-02-05 14:46:09 +0700

    first update

--- app/src/main/java/com/example/yetanotherfragment/MainActivity.kt
+++ app/src/main/java/com/example/yetanotherfragment/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.yetanotherfragment
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,34 +17,76 @@ class MainActivity : AppCompatActivity() {
         val To = findViewById<Button>(R.id.To)
         val Tpfl = findViewById<Button>(R.id.Tpfl)
 
+        Toast.makeText(this, "App is running", Toast.LENGTH_SHORT).show()
+
         Busana.setOnClickListener {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentcontainer, BusanaFragment())
             fragmentTransaction.commit()
+
+            AlertDialog.Builder(this)
+                .setTitle("Important")
+                .setMessage("Coblos 02 all in")
+                .setPositiveButton("Yes",{ dialogInterface, i -> Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show() })
+                .setNegativeButton("No",{ dialogInterface, i -> Toast.makeText(this, "Fragment not running", Toast.LENGTH_SHORT).show() })
+
+//            Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show()
         }
 
         Kuliner.setOnClickListener {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentcontainer, KulinerFragment())
             fragmentTransaction.commit()
+
+            AlertDialog.Builder(this)
+                .setTitle("Important")
+                .setMessage("Coblos 02 all in")
+                .setPositiveButton("Yes",{ dialogInterface, i -> Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show() })
+                .setNegativeButton("No",{ dialogInterface, i -> Toast.makeText(this, "Fragment not running", Toast.LENGTH_SHORT).show() })
+
+//            Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show()
         }
 
         Pplg.setOnClickListener {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentcontainer, PplgFragment())
             fragmentTransaction.commit()
+
+            AlertDialog.Builder(this)
+                .setTitle("Important")
+                .setMessage("Coblos 02 all in")
+                .setPositiveButton("Yes",{ dialogInterface, i -> Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show() })
+                .setNegativeButton("No",{ dialogInterface, i -> Toast.makeText(this, "Fragment not running", Toast.LENGTH_SHORT).show() })
+
+//            Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show()
         }
 
         To.setOnClickListener {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentcontainer, ToFragment())
             fragmentTransaction.commit()
+
+            AlertDialog.Builder(this)
+                .setTitle("Important")
+                .setMessage("Coblos 02 all in")
+                .setPositiveButton("Yes",{ dialogInterface, i -> Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show() })
+                .setNegativeButton("No",{ dialogInterface, i -> Toast.makeText(this, "Fragment not running", Toast.LENGTH_SHORT).show() })
+
+//            Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show()
         }
 
         Tpfl.setOnClickListener {
             val fragmentTransaction = supportFragmentManager.beginTransaction()
             fragmentTransaction.replace(R.id.fragmentcontainer, TpflFragment())
             fragmentTransaction.commit()
+
+            AlertDialog.Builder(this)
+                .setTitle("Important")
+                .setMessage("Coblos 02 all in")
+                .setPositiveButton("Yes",{ dialogInterface, i -> Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show() })
+                .setNegativeButton("No",{ dialogInterface, i -> Toast.makeText(this, "Fragment not running", Toast.LENGTH_SHORT).show() })
+
+//            Toast.makeText(this, "Fragment is running", Toast.LENGTH_SHORT).show()
         }
 
     }
